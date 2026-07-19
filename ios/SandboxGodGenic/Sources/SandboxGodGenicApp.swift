29b0fe961fba6c9c7043cacb67078706f80e709a
Toymaker474 <toymaker474@gmail.com>
2026-07-19 19:03:37 -0400

    Add SwiftUI app entry point

---
+++ ios/SandboxGodGenic/Sources/SandboxGodGenicApp.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+@main
+struct SandboxGodGenicApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    @State private var engine = SimulationEngine()
+
+    var body: some Scene {
+        WindowGroup {
+            RootView(engine: engine)
+                .preferredColorScheme(.dark)
+                .task { engine.start() }
+                .onChange(of: scenePhase) { _, phase in
+                    engine.handleScenePhase(phase)
+                }
+        }
+    }
+}
