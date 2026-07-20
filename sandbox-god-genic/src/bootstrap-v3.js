8b059c5af31295ede4e97fa2a2128ad076c3fb15
Toymaker474 <toymaker474@gmail.com>
2026-07-19 20:20:26 -0400

    Force PWA v3 service worker refresh

--- sandbox-god-genic/src/bootstrap-v3.js
+++ sandbox-god-genic/src/bootstrap-v3.js
@@ -1,3 +1,6 @@
 import'./upgrade-v3.js?v=3';
 await import('./main.js?v=3');
+if('serviceWorker'in navigator){
+  navigator.serviceWorker.register('./sandbox-god-genic/sw.js?v=3',{updateViaCache:'none'}).catch(()=>{});
+}
 window.dispatchEvent(new Event('godgenic:booted'));
