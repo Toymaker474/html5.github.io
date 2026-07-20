99a0bcf3cda7c977a228c8b924f7a15539950d1d
Toymaker474 <toymaker474@gmail.com>
2026-07-19 20:46:12 -0400

    Boot the alien ecosystem v4 before the main engine

--- sandbox-god-genic/src/bootstrap-v3.js
+++ sandbox-god-genic/src/bootstrap-v3.js
@@ -1,6 +1,7 @@
 import'./upgrade-v3.js?v=3';
-await import('./main.js?v=3');
+import'./ecosystem-v4.js?v=4';
+await import('./main.js?v=4');
 if('serviceWorker'in navigator){
-  navigator.serviceWorker.register('./sandbox-god-genic/sw.js?v=3',{updateViaCache:'none'}).catch(()=>{});
+  navigator.serviceWorker.register('./sandbox-god-genic/sw.js?v=4',{updateViaCache:'none'}).catch(()=>{});
 }
 window.dispatchEvent(new Event('godgenic:booted'));
