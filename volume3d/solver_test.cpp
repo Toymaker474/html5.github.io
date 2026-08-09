36a8cd01b6a8aedf0a88e53f915e89b00d884bca
Toymaker474 <toymaker474@gmail.com>
2026-08-09 09:45:02 -0400

    Add native C++ volumetric regression fixture

---
+++ volume3d/solver_test.cpp
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include <stdio.h>
+#include "solver.cpp"
+int main(){
+  genesis_init(32,24,32,123456u,72);genesis_seed_scene();
+  uint32_t h0=genesis_hash(),s0=genesis_sand_mass_units(),w0=genesis_water_cells();
+  for(int i=0;i<80;i++)genesis_step(1);
+  uint32_t h1=genesis_hash(),s1=genesis_sand_mass_units(),w1=genesis_water_cells();
+  assert(genesis_invariant()==1);assert(s0==s1);assert(w0==w1);assert(h0!=h1);
+  uint32_t e=genesis_erosion_events(),d=genesis_deposition_events();assert(e>0);assert(d>0);
+  genesis_init(32,24,32,123456u,72);genesis_seed_scene();for(int i=0;i<80;i++)genesis_step(1);assert(h1==genesis_hash());
+  genesis_init(24,20,24,9u,95);genesis_clear();int made=genesis_paint(5,12,15,12,4);assert(made>0);uint32_t wet0=genesis_sand_mass_units();for(int i=0;i<30;i++)genesis_step(1);assert(genesis_sand_mass_units()==wet0);assert(genesis_invariant()==1);
+  printf("{\"model\":\"genesis-materials-volume3d-cpp-v1\",\"hash\":%u,\"sandUnits\":%u,\"waterCells\":%u,\"erosion\":%u,\"deposition\":%u}\n",h1,s1,w1,e,d);
+  puts("PASS genesis materials 0.3 native C++ volumetric tests");
+}
