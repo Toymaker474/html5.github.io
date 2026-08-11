f481f196545bc1b87a3d93636721fb09dd89271d
Toymaker474 <toymaker474@gmail.com>
2026-08-11 08:58:21 -0400

    Export WASM heap for mobile viewer

--- ouroboros-kernel-v1/build-web.sh
+++ ouroboros-kernel-v1/build-web.sh
@@ -8,5 +8,6 @@ em++ native/material_kernel.cpp -O3 -std=c++20 \
   -sEXPORT_NAME=createOuroborosKernel \
   -sALLOW_MEMORY_GROWTH=1 \
   -sENVIRONMENT=web \
+  -sEXPORTED_RUNTIME_METHODS="['HEAPF32']" \
   -sEXPORTED_FUNCTIONS="['_ok_reset','_ok_step','_ok_inject_water','_ok_inject_lava','_ok_n','_ok_terrain','_ok_sand','_ok_water','_ok_lava','_ok_lava_temp','_ok_steam','_ok_sediment','_ok_uw','_ok_vw','_ok_mass_water','_ok_mass_rock','_ok_evaporated','_ok_solidified','_ok_eroded','_ok_ticks']" \
   -o kernel.js
