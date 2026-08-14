16ba77502b1403d7f1de1fa13097516a4394166e
Toymaker474 <toymaker474@gmail.com>
2026-08-14 18:16:17 -0400

    Keep freestanding memory shims WASM-only

--- volume3d/solver.cpp
+++ volume3d/solver.cpp
@@ -5,8 +5,10 @@
 #else
 #define EXPORT(name)
 #endif
+#if defined(__wasm__)
 extern "C" void* memset(void* dst,int value,size_t n){unsigned char* p=(unsigned char*)dst;for(size_t i=0;i<n;i++)p[i]=(unsigned char)value;return dst;}
 extern "C" void* memcpy(void* dst,const void* src,size_t n){unsigned char* d=(unsigned char*)dst;const unsigned char* s=(const unsigned char*)src;for(size_t i=0;i<n;i++)d[i]=s[i];return dst;}
+#endif
 namespace {
 constexpr int MAX_W=64,MAX_H=40,MAX_D=64,MAX_CELLS=MAX_W*MAX_H*MAX_D;
 enum Material:uint8_t{EMPTY=0,SAND=1,WATER=2,ROCK=3};
